03d99c0291e9316e2221f54e3bbc689a4a2a5467
Jakub Kasprzyk <neus90@gmail.com>
2016-01-21 17:47:10 +0100

    use arrow functions

--- src/controllers/home.controller.test.js
+++ src/controllers/home.controller.test.js
@@ -1,24 +1,24 @@
 import app from '../app';
 
-describe('Controller: HomeController', function() {
+describe('Controller: HomeController', () => {
   let $controller;
 
   beforeEach(angular.mock.module(app));
-  beforeEach(angular.mock.inject(function(_$controller_) {
+  beforeEach(angular.mock.inject(_$controller_ => {
     $controller = _$controller_;
   }));
 
-  it('should expose `toggleSelect` method', function() {
+  it('should expose `toggleSelect` method', () => {
     let ctrl = $controller('HomeController');
     expect(ctrl.toggleSelect).toBeDefined();
   });
 
-  it('should expose `setSelected` method', function() {
+  it('should expose `setSelected` method', () => {
     let ctrl = $controller('HomeController');
     expect(ctrl.setSelected).toBeDefined();
   });
 
-  it('should expose `removeSelected` method', function() {
+  it('should expose `removeSelected` method', () => {
     let ctrl = $controller('HomeController');
     expect(ctrl.removeSelected).toBeDefined();
   });
