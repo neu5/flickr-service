e09fbf12c91c1ef644bdeb969b28ceabd631db35
Jakub Kasprzyk <neus90@gmail.com>
2016-01-27 00:57:55 +0100

    save the 'selected'

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,13 +1,28 @@
 export default class HomeController {
   constructor(GetFlickrItems, $cookies) {
     this.$cookies = $cookies;
+    this.flickrObj = [];
+
+    // this is just a hack, because Angular adds some debug keys
+    this.removeNotNeededKeys();
+
+    if (Object.keys(this.$cookies.getAll()).length > 0) {
+      Object.keys(this.$cookies.getAll()).forEach(elem => {
+        this.flickrObj.push({
+          link: elem,
+          media: {
+            m: this.$cookies.get(elem)
+          },
+          isSelected: true
+        });
+      });
+    }
 
     GetFlickrItems.then(data => {
-      this.flickrObj = data.items.map(elem => {
-        if (this.$cookies.get(elem.link)) {
-          this.setSelected(elem);
+      data.items.forEach(elem => {
+        if (this.$cookies.get(elem.link) === undefined) {
+          this.flickrObj.push(elem);
         }
-        return elem;
       });
     });
   }
@@ -21,13 +36,25 @@ export default class HomeController {
   }
 
   setSelected(elem) {
-    this.$cookies.put(elem.link, true);
+    this.$cookies.put(elem.link, elem.media.m);
     elem.isSelected = true;
   }
 
   removeSelected(elem) {
     this.$cookies.remove(elem.link);
     elem.isSelected = false;
+  }
+
+  removeNotNeededKeys() {
+    if (this.$cookies.get('__Debug') !== 'undefined') {
+      this.$cookies.remove('__Debug');
+    }
+    if (this.$cookies.get('csrftoken') !== 'undefined') {
+      this.$cookies.remove('csrftoken');
+    }
+    if (this.$cookies.get('__ngDebug') !== 'undefined') {
+      this.$cookies.remove('__ngDebug');
+    }
   }
 }
 
